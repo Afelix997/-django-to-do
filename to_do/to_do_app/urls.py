8926dfb4ce24627262235cfe1543d998cf0c49e1
afelix997 <afelix2013@gmail.com>
2022-07-12 23:03:01 -0700

    finished

--- to_do/to_do_app/urls.py
+++ to_do/to_do_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path("", views.index),
     path("todo/", views.todo),
     path("todo/new/", views.todo_new),
+    path("todo/<int:todo_id>/", views.todo_detail),
+    path("todo/<int:todo_id>/edit/", views.todo_edit),
+    path("todo/<int:todo_id>/edit_/", views.todo_edit_),
+    path("todo/<int:todo_id>/delete/", views.todo_delete),
     ]
--- to_do/to_do_app/views.py
+++ to_do/to_do_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse,  JsonResponse
 from .models import Todo
 
@@ -13,10 +13,28 @@ def todo(request):
             data = request.POST
             Todo.objects.create(title=data['title'], desc=data['desc'])
             return render(request, 'to_do_app/todo.html', { 'context':Todo.objects.all()})
-        except Exception as e:
-            print(str(e))
+        except Exception as error:
+            print(str(error))
             return JsonResponse({'success':False})
-    
     
 def todo_new(request):
     return render(request, 'to_do_app/todo_new.html')
+
+def get_todo(todo_id):
+    return Todo.objects.get(id = todo_id)
+
+def todo_detail(request,todo_id):
+    todo_item=get_todo(todo_id)
+    return  render(request, 'to_do_app/todo_detail.html', {'item': todo_item})
+def todo_edit(request,todo_id):
+    todo_item=get_todo(todo_id)
+    return  render(request, 'to_do_app/todo_edit.html', {'item': todo_item})
+def todo_edit_(request,todo_id):
+    data = request.POST
+    Todo.objects.filter(id=todo_id).update(title=data['title'], desc=data['desc'])
+    return redirect('/todo/')
+def todo_delete(request,todo_id):
+    data = request.POST
+    Todo.objects.filter(id=data['id']).delete()
+    return redirect('/todo/')
+   
